75fbc00c0fa3b9b9c12501a723f31c003ff340cc
Jaenudin Firdaus <2022110103@students.uigm.ac.id>
2025-02-11 17:57:16 +0700

    styling navbar component

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,20 +2,42 @@ import { NavLink } from "react-router-dom";
 
 function NavBar() {
   return (
-    <nav>
-      <NavLink to="/">JayNode</NavLink>
-      <ul>
-        <li>
-          <NavLink to="about">About</NavLink>
+    <nav className="navbar container">
+      <NavLink to="/" className="fw-bold fs-2 text-decoration-none">
+        JayNode
+      </NavLink>
+      <ul className="navbar-nav d-flex flex-row gap-4">
+        <li className="nav-item">
+          <NavLink
+            to="/about"
+            className="text-decoration-none fs-5 fw-semibold"
+          >
+            About
+          </NavLink>
         </li>
-        <li>
-          <NavLink to="project">Project</NavLink>
+        <li className="nav-item">
+          <NavLink
+            to="/project"
+            className="text-decoration-none fs-5 fw-semibold"
+          >
+            Project
+          </NavLink>
         </li>
-        <li>
-          <NavLink to="contact">Contact</NavLink>
+        <li className="nav-item">
+          <NavLink
+            to="/contact"
+            className="text-decoration-none fs-5 fw-semibold"
+          >
+            Contact
+          </NavLink>
         </li>
-        <li>
-          <NavLink to="login">Login</NavLink>
+        <li className="nav-item">
+          <NavLink
+            to="/login"
+            className="text-decoration-none fs-5 fw-semibold"
+          >
+            Login
+          </NavLink>
         </li>
       </ul>
     </nav>
