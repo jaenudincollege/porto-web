64187d5d190dd0c4fcf64d204b3746a039f6c5fa
Jaenudin Firdaus <2022110103@students.uigm.ac.id>
2025-02-11 18:16:03 +0700

    change input type to password

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -9,17 +9,17 @@ function Login() {
 
         <form className="d-flex flex-column gap-1">
           <input
-            type="text"
-            name="username"
-            id="username"
-            placeholder="username"
-            className="form-control fs-4"
-          />
-          <input
             type="email"
             name="email"
             id="email"
             placeholder="email"
+            className="form-control fs-4"
+          />
+          <input
+            type="password"
+            name="password"
+            id="password"
+            placeholder="password"
             className="form-control fs-4"
           />
           <button className="btn btn-primary fw-semibold fs-5">Log in</button>
