841bfb6ea55097f9bf6fac88b12aef197799e5a6
Jaenudin Firdaus <2022110103@students.uigm.ac.id>
2025-02-11 17:57:06 +0700

    styling login page

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,10 +1,31 @@
+import NavBar from "./NavBar";
+
 function Login() {
   return (
-    <form>
-      <input type="text" name="username" id="username" />
-      <input type="email" name="email" id="email" />
-      <button>Log in</button>
-    </form>
+    <>
+      <NavBar />
+      <div className="container mt-5">
+        <h1 className="text-center my-5 fw-bold">Login</h1>
+
+        <form className="d-flex flex-column gap-1">
+          <input
+            type="text"
+            name="username"
+            id="username"
+            placeholder="username"
+            className="form-control fs-4"
+          />
+          <input
+            type="email"
+            name="email"
+            id="email"
+            placeholder="email"
+            className="form-control fs-4"
+          />
+          <button className="btn btn-primary fw-semibold fs-5">Log in</button>
+        </form>
+      </div>
+    </>
   );
 }
 
