7db5bd7f218c90e0ff3cef2d75b7d4add124e5d5
Jaenudin Firdaus <2022110103@students.uigm.ac.id>
2025-02-11 17:12:40 +0700

    create homepage component

---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,15 @@
+import NavBar from "../components/NavBar";
+
+function Homepage() {
+  return (
+    <div>
+      <header>
+        <NavBar />
+      </header>
+
+      <main></main>
+    </div>
+  );
+}
+
+export default Homepage;
