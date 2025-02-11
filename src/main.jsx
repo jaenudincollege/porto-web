b8e4add8bb9b41f08db559086793ca216ffabc5e
Jaenudin Firdaus <2022110103@students.uigm.ac.id>
2025-02-11 17:31:40 +0700

    implement dynamic routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
+import App from "./App.jsx";
+import Login from "./components/Login.jsx";
+import About from "./components/About.jsx";
+import Project from "./components/Project.jsx";
+import Contact from "./components/Contact.jsx";
+
+const router = createBrowserRouter([
+  { path: "/", element: <App /> },
+  { path: "about", element: <About /> },
+  { path: "project", element: <Project /> },
+  { path: "contact", element: <Contact /> },
+  { path: "login", element: <Login /> },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
