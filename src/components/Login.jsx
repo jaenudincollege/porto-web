ad4d654488e01c2054ecdee70cda8f9f1cbcd7b5
Jaenudin Firdaus <2022110103@students.uigm.ac.id>
2025-02-11 17:29:43 +0700

    create login component

---
+++ src/components/Login.jsx
@@ -0,0 +1,11 @@
+function Login() {
+  return (
+    <form>
+      <input type="text" name="username" id="username" />
+      <input type="email" name="email" id="email" />
+      <button>Log in</button>
+    </form>
+  );
+}
+
+export default Login;
