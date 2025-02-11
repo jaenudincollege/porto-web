7b1960848b58730f34b6d6e1a4dde3e0d77fc38b
Jaenudin Firdaus <2022110103@students.uigm.ac.id>
2025-02-11 18:34:58 +0700

    change repeatable code to a component

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,10 +1,22 @@
+import Card from "./Card";
 import NavBar from "./NavBar";
 
 function Project() {
   return (
     <>
       <NavBar />
-      <div>Project</div>
+      <div className="album py-5 bg-body-tertiary">
+        <div className="container">
+          <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+          </div>
+        </div>
+      </div>
     </>
   );
 }
