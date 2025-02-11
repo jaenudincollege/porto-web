4c4c8297e9336547e0611a1994cb2033bb950974
Jaenudin Firdaus <2022110103@students.uigm.ac.id>
2025-02-11 17:11:26 +0700

    create navbar component

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,15 @@
+function NavBar() {
+  return (
+    <nav>
+      <span>JayNode</span>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Work</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default NavBar;
