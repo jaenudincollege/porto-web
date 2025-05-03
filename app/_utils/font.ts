f2f83b910f99bd95eb1f7712ea518000f6227ea4
Jaenudin Firdaus <2022110103@students.uigm.ac.id>
2025-05-03 14:53:40 +0700

    Refactoring font

--- app/_utils/font.ts
+++ app/_utils/font.ts
@@ -1,4 +1,9 @@
-import { Rouge_Script } from "next/font/google";
+import { Roboto, Rouge_Script } from "next/font/google";
+
+export const roboto = Roboto({
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  subsets: ["latin"],
+});
 
 export const rs = Rouge_Script({
   weight: ["400"],
